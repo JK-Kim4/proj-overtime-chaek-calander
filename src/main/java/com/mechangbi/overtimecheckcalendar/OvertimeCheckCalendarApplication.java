9b4d5f09c470746301ddd4d88aef4b13fddc3887
jongwan <jongbell4@gmail.com>
2024-07-16 10:45:10 +0900

    first init

---
+++ src/main/java/com/mechangbi/overtimecheckcalendar/OvertimeCheckCalendarApplication.java
@@ -0,0 +1,13 @@
+package com.mechangbi.overtimecheckcalendar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OvertimeCheckCalendarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OvertimeCheckCalendarApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mechangbi/overtimecheckcalendar/OvertimeCheckCalendarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mechangbi.overtimecheckcalendar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OvertimeCheckCalendarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
